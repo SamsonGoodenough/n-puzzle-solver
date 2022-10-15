9c212fa93a15ef38fb154cda9773c9e93218f430
Samson Goodenough <goodenough.samson@gmail.com>
2022-10-15 00:14:59 -0400

    Add documentation and arguments
    Co-authored-by: Riley Huston <mr-rjh3@users.noreply.github.com>

--- lib.py
+++ lib.py
@@ -1,22 +1,197 @@
-import math
-import random
+import math, random, heapq, time
+
 class Heuristics:
   def _displacementHeuristic(state):
-    pass
+    """
+    ----------------------------------------------------------
+    Description: Calculate the number of misplaced tiles in the state.
+    Use: puzzles = Puzzle.randomizePuzzle(Heuristics.MISPLACED, 8)
+    ----------------------------------------------------------
+    Returns:
+      count - The number of misplaced tiles in the state.
+    ----------------------------------------------------------
+    """
+    count = 0
+    for i in range(state.boardLength):
+      if state.board[i] != 0 and state.board[i] != i:
+        count += 1 
+    return count
   
   def _manhattanHeuristic(state):
+    """
+    ----------------------------------------------------------
+    Description: Calculate the manhattan distance of the state.
+    Use: puzzles = Puzzle.randomizePuzzle(Heuristics.MANHATTAN, 8)
+    ----------------------------------------------------------
+    Parameters:
+      state - The state to calculate the manhattan distance of.
+    Returns:
+      h - The manhattan distance of the state.
+    ----------------------------------------------------------
+    """
     h = 0
     for i in range(state.boardLength):
       if state.board[i] != 0:
         x1, y1 = state._get2dCoord(i)
-        x2, y2 = state._get2dCoord(state.board[i])
+        x2, y2 = state._get2dCoord(state.board[i])  
         h += abs(x1 - x2) + abs(y1 - y2)
     return h
   
   DISPLACEMENT = _displacementHeuristic
   MANHATTAN = _manhattanHeuristic
   
-  
+
+class Puzzle:
+  def randomizePuzzles(heuristic, size):
+    """
+    ----------------------------------------------------------
+    Description: Randomizes 100 puzzles of the given size.
+    Use: puzzles = Puzzle.randomizePuzzles(Heuristics.MANHATTAN, 8)
+    ----------------------------------------------------------
+    Parameters:
+      heuristic - The heuristic to use for the puzzles.
+      size - The size of the puzzle to randomize. Must be a perfect square.
+    Returns:
+      puzzles - Array of randomized puzzles.
+    ----------------------------------------------------------
+    """
+    try:
+      assert math.sqrt(size + 1) % 1 == 0
+      puzzles = []
+      while len(puzzles) < 100:
+        board = [i for i in range(size+1)]
+        random.shuffle(board)
+        s = State(board, math.sqrt(len(board)), heuristic=heuristic)
+        if (s.isSolvable()):
+          puzzles.append(s)
+      return puzzles
+    except AssertionError:
+      print("ERROR:\t Size must be a perfect square!")
+      return
+    
+  def solvePuzzleArray(puzzles, outputFile, outputCSV, debug=False):
+    """
+    ----------------------------------------------------------
+    Description: Solves the given puzzles and writes the results to the given file.
+    Use: Puzzle.solvePuzzles(puzzles)
+    ----------------------------------------------------------
+    Parameters:
+      puzzles - An array of puzzles to solve.
+    Yields:
+      count - The current number of puzzles solved.
+    ----------------------------------------------------------
+    """
+    count = 1
+    totalStats = {
+      'timeTaken': 0,
+      'numNodesExplored': 0,
+      'numStepsToSolution': 0,
+      'nodesPerSecond': 0
+    }
+    file = open(outputFile, "w")
+    fStats = open(outputCSV, "w")
+    fStats.write("Puzzle ID,Puzzle,Time Taken,Nodes Explored,Steps to Solution,Nodes per Second") # write header
+    for puzzle in puzzles:
+      # print("Puzzle #", count)
+      stats = Puzzle.solvePuzzle(puzzle, debug)
+      
+      # Update total stats
+      totalStats["numNodesExplored"] += stats["numNodesExplored"]
+      totalStats["timeTaken"] += stats["timeTaken"]
+      totalStats["nodesPerSecond"] += stats["nodesPerSecond"]
+      totalStats["numStepsToSolution"] += len(stats["pathToSolution"])
+      
+      # Write to file
+      file.write("-"*150 + "\n")
+      file.write(" Puzzle # " + str(count) + "\n")
+      file.write("\t{:<30} ---> {:.2f}s \n".format("Time taken to complete puzzle:",stats["timeTaken"]))
+      file.write("\t{:<30} ---> {} \n".format("Number of expanded nodes:",str(stats["numNodesExplored"])))
+      file.write("\t{:<30} ---> {} \n".format("Number of steps to solution:",str(len(stats["pathToSolution"]))))
+      file.write("\t{:<30} ---> {} \n".format("Path to Solution:",str(stats["pathToSolution"])))
+      file.write("\t{:<30} ---> {:.2f} nodes/s \n".format("Nodes expanded per second:",stats["nodesPerSecond"]))
+      fStats.write('%s,%s,%s,%s,%s,%s' % (str(count),str(stats["startingBoard"]),str(stats["timeTaken"]),str(stats["numNodesExplored"]),str(len(stats["pathToSolution"])),str(stats["nodesPerSecond"])))
+      yield 
+      count += 1
+      file.flush()
+      fStats.flush()
+      
+    # Write total stats to file
+    file.write("\n" + "="*150 + "\n")  
+    file.write(" Average Stats for 100 {}-Puzzles: \n".format(str(puzzles[0].boardLength-1)))
+    file.write("\t{:<38} ---> {:.2f}s \n".format("Average Time taken to complete puzzle:",totalStats["timeTaken"]/len(puzzles)))
+    file.write("\t{:<38} ---> {} \n".format("Average Number of expanded nodes:",str(totalStats["numNodesExplored"]/len(puzzles))))
+    file.write("\t{:<38} ---> {} \n".format("Average Number of steps to solution:",str(totalStats["numStepsToSolution"]/len(puzzles))))
+    file.write("\t{:<38} ---> {:.2f} nodes/s \n".format("Average Nodes expanded per second:",totalStats["nodesPerSecond"]/len(puzzles)))
+    file.write("="*150 + "\n")  
+
+    file.close()
+    fStats.close()
+    # print average stats
+    print("="*150)  
+    print(" Average Stats for 100 {}-Puzzles:".format(str(puzzles[0].boardLength-1)))
+    print("\t{:<38} ---> {:.2f}s".format("Average Time taken to complete puzzle:",totalStats["timeTaken"]/len(puzzles)))
+    print("\t{:<38} ---> {}".format("Average Number of expanded nodes:",str(totalStats["numNodesExplored"]/len(puzzles))))
+    print("\t{:<38} ---> {}".format("Average Number of steps to solution:",str(totalStats["numStepsToSolution"]/len(puzzles))))
+    print("\t{:<38} ---> {:.2f} nodes/s".format("Average Nodes expanded per second:",totalStats["nodesPerSecond"]/len(puzzles)))
+    print("="*150)  
+    
+    return
+    
+  def solvePuzzle(s, debug=False):
+    """
+    ----------------------------------------------------------
+    Description: Solves the given puzzle using A*.
+    Use: stats = Puzzle.solvePuzzle(puzzle)
+    ----------------------------------------------------------
+    Parameters:
+      s - Starting state to solve the puzzle from.
+      debug - Whether or not to print debug information.
+    Returns:
+      stats - Dictionary containing the following information:
+        timeTaken         - The time taken to solve the puzzle.
+        numNodesExplored  - The number of nodes explored.
+        pathToSolution    - The path to the solution.
+        nodesPerSecond    - The number of nodes expanded per second.
+    ----------------------------------------------------------
+    """
+    explored = {}
+    frontier = []
+    startingBoard = s
+    start = time.time()
+    heapq.heapify(frontier) # create heap and add initial state
+    heapq.heappush(frontier, s)
+
+    if debug: print('start:\t', str(s))
+    while len(frontier) != 0: # loop until frontier is empty
+      s = heapq.heappop(frontier)
+      exploredCost = explored.get(str(s))
+      if exploredCost != None:
+        if exploredCost <= s.g: # if the explored cost is less than the current cost, then we don't need to explore this state
+          continue
+      explored[str(s)] = s.g
+      
+      if s.h == 0: # check if at the goal state
+        break
+      
+      if debug:
+        if len(explored) % 100000 == 0:
+          print('#explored:\t', len(explored))
+          print('nps:\t\t {:.2f}\n'.format(len(explored) / (time.time() - start)))
+        
+      for move in s.getMoves(): # add all possible moves to the frontier
+        heapq.heappush(frontier, move)
+
+    # pass the solution and stats back to the caller
+    end = time.time()
+    stats = {
+      'timeTaken': (time.time() - start),
+      'numNodesExplored': len(explored),
+      'pathToSolution': s.path[:],
+      'nodesPerSecond': len(explored) / ((end - start) if (end - start) != 0 else 0.01),
+      'startingBoard': str(startingBoard)
+    }
+    if debug: print('done:\t', str(s))
+    return stats
 
 class State:
   def __init__(self, board, width, g=0, path = [], heuristic=Heuristics.MANHATTAN):
@@ -50,6 +225,12 @@ class State:
     return (index % self.width, index // self.width)
   
   def getMoves(self):
+    """
+    ----------------------------------------------------------
+    Description - Finds the states that are reachable from self state.
+    Use: State.getMoves()
+    ----------------------------------------------------------
+    """
     moves = []
     index = self.board.index(0)
     row = index // self.width
--- main.py
+++ main.py
@@ -1,47 +1,29 @@
-from lib import State, Heuristics
-import heapq
-import math
-import random
-import time
+from lib import Heuristics, Puzzle
+from alive_progress import alive_bar
+import argparse
 
-start = time.time()
+parser = argparse.ArgumentParser(description='A* algorithm for n-puzzle')
+parser.add_argument("-H", "--heuristic", help="The heuristic to use", choices=["manhattan", "displacement"], default="manhattan")
+parser.add_argument("-s", "--size", help="The size of the puzzle e.g. 8 for 8-puzzle (3x3)", choices=[8, 15, 24, 35], type=int, default=8)
+parser.add_argument("-o", "--outputFile", help="Supplies the file name to output text to.", default="output.txt")
+parser.add_argument("-csv", "--outputCSV", help="Supplies the file name to output to csv data to.", default="stats.csv")
+parser.add_argument("-d", "--debug", help="Supplies the file name to output to csv data to.", action="store_true")
+args = parser.parse_args()
 
-startingBoard = [i for i in range(9)]
-# startingBoard = [1, 2, 0, 3, 4, 5, 6, 7, 8]
-# startingBoard = [i for i in range(16)]
-# startingBoard = [4, 1, 2, 3, 5, 6, 7, 11, 8, 9, 10, 0, 12, 13, 14, 15]
-random.shuffle(startingBoard)
-s = State(startingBoard, math.sqrt(len(startingBoard)), heuristic=Heuristics.MANHATTAN)
-while s.isSolvable() == False:
-  random.shuffle(startingBoard)
-  s = State(startingBoard, math.sqrt(len(startingBoard)), heuristic=Heuristics.MANHATTAN)
+if(args.heuristic == "manhattan"):
+  heuristic = Heuristics.MANHATTAN
+elif(args.heuristic == "displacement"):
+  heuristic = Heuristics.DISPLACEMENT
   
-explored = {}
-frontier = []
-heapq.heapify(frontier)
-heapq.heappush(frontier, s)
+# elif(args.heuristic == "linearConflict"): #TODO you should make yourself NOW!
+  # heuristic = Heuristics.linearConflict
+  
+puzzles = Puzzle.randomizePuzzles(heuristic, args.size)
 
-print('start:\t', str(s))
-while len(frontier) != 0:
-  s = heapq.heappop(frontier)
-  if explored.get(str(s)) != None:
-    continue
-  explored[str(s)] = True
-  
-  if s.h == 0:
-    break
-  
-  if len(explored) % 100000 == 0:
-    print('#explored:\t', len(explored))
-    print('nps:\t\t {:.2f}\n'.format(len(explored) / (time.time() - start)))
-    
-  
-  for move in s.getMoves():
-    if str(move) not in explored:
-      heapq.heappush(frontier, move)
-
-print('done:\t', str(s))
-print('#explored:', len(explored))
-print('#path:', '->'.join(map(str, s.path)))
-print('nps:\t\t {:.2f}'.format(len(explored) / (time.time() - start)))
-print('total time:\t {:.2f}\n'.format((time.time() - start)))
+if(not args.debug):
+  with alive_bar(100, enrich_print=False) as bar:
+    for i in Puzzle.solvePuzzleArray(puzzles, args.outputFile, args.outputCSV):
+      bar()
+else:
+  for i in Puzzle.solvePuzzleArray(puzzles, args.outputFile, args.outputCSV, True):
+    pass
